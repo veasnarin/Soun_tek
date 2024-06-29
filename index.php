e2cd3061f0829d9dbd734004ed038110da6d5414
udom <ngetudom9999@gmail.com>
2024-06-29 08:44:48 +0700

    update with new SUBMIT

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 <?php
 include_once("actions/db.php");
 $db = new Database();
+if (isset($_POST['id_nbr']) && isset($_POST['code_nbr'])) {
+  $con = "cus_products.nbr_id='" . $_POST['id_nbr'] . "' AND cus_products.code='" . $_POST['code_nbr'] . "'";
+  $result = $db->getProductByCode($con);
+}
+
 ?>
 <html lang="en" class="light-style layout-navbar-fixed layout-wide" dir="ltr" data-theme="theme-default" data-assets-path="assets/" data-template="front-pages">
 
@@ -9,7 +14,7 @@ $db = new Database();
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
   <meta name="description" content="" />
-  <link rel="icon" type="image/x-icon" href="<?= $db->url(); ?>/SUNFIX/Soun_tek/assets/vendor/img/favicon/favicon.ico" />
+  <link rel="icon" type="image/x-icon" href="<?= $db->url(); ?>/SUNFIX/Soun_tek/assets/vendor/img/favicon/stco.jpg" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap" rel="stylesheet" />
@@ -20,7 +25,7 @@ $db = new Database();
   <script src="<?= $db->url(); ?>/SUNFIX/Soun_tek/assets/vendor/js/helpers.js"></script>
   <script src="<?= $db->url(); ?>/SUNFIX/Soun_tek/assets/vendor/js/template-customizer.js"></script>
   <script src="<?= $db->url(); ?>/SUNFIX/Soun_tek/assets/vendor/js/front-config.js"></script>
-  <title>Front Pages</title>
+  <title><?= isset($_POST['id_nbr']) && isset($_POST['code_nbr']) && $result == true ? $result['name'] : 'STCO' ?></title>
 </head>
 
 <body>
@@ -37,7 +42,7 @@ $db = new Database();
           <!-- Mobile menu toggle: End-->
           <a href="landing-page.html" class="app-brand-link">
             <span>
-              <img src="<?= $db->url(); ?>/SUNFIX/Soun_tek/assets/vendor/img/logos/logo-1.png" alt="" width="130">
+              <img src="<?= $db->url(); ?>/SUNFIX/Soun_tek/assets/vendor/img/logos/STCO.jpg" alt="" width="130">
             </span>
           </a>
         </div>
@@ -118,6 +123,16 @@ $db = new Database();
             </h2>
             <div class="card">
               <div class="card-body">
+                <?php
+                if (isset($result) && $result != TRUE) {
+                ?>
+                  <div class="alert alert-danger alert-dismissible" role="alert">
+                    Product Not Found.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                  </div>
+                <?php
+                }
+                ?>
                 <form action="index.php" method="POST">
                   <div class="row g-3">
                     <div class="col-md-6">
@@ -140,141 +155,133 @@ $db = new Database();
     <!-- Hero: End -->
     <!-- Results: Start -->
     <?php
-    if (isset($_POST['id_nbr']) && isset($_POST['code_nbr'])) {
-      $con = "cus_products.nbr_id='" . $_POST['id_nbr'] . "' AND cus_products.code='" . $_POST['code_nbr'] . "'";
-      $result = $db->getProductByCode($con);
-      if ($result) {
+    if (isset($_POST['id_nbr']) && isset($_POST['code_nbr']) && $result) {
     ?>
-        <section id="landingContact" class="section-py bg-body landing-contact">
-          <div class="container">
-            <div class="col-lg-12">
-              <h4 class="text-center">RESULTS</h4>
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="mb-3 float-end">ONLINE VERIFICATION SERVICES - GFCO GEM LAB*</h5><br>
-                  <table>
-                    <tr>
-                      <td class="pe-4">REPORT ID NBR</td>
-                      <td>:</td>
-                      <td class="ps-2 fw-bold text-black"><?= $result['nbr_id']; ?></td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4">CODE</td>
-                      <td>:</td>
-                      <td class="ps-2"><?= $result['code']; ?></td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4">DATE (DD/MM/YY)</td>
-                      <td>:</td>
-                      <td class="ps-2"><?= $db->date_format($result['date']); ?></td>
-                    </tr>
-                    <tr>
-                      <td class="pt-3"><img src="<?= $result['image_link'] ?>" alt="" width="150"></td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4 pt-3">FAMILY/SPECIE</td>
-                      <td class="pt-3">:</td>
-                      <td class="ps-2 pt-3 fw-bold text-black"><?= $result['cate_name'] ?></td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4">TRADE NAME</td>
-                      <td>:</td>
-                      <td class="ps-2 text-danger fw-bold"><?= $result['name'] ?></td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4">WEIGHT</td>
-                      <td>:</td>
-                      <td class="ps-2"><?= $result['p_weight'] ?> CARATS</td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4">COLOR</td>
-                      <td>:</td>
-                      <td class="ps-2"><?= $result['var_name'] ?></td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4">SHAPE</td>
-                      <td>:</td>
-                      <td class="ps-2"><?= $result['shape'] ?></td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4">DEMESIONS</td>
-                      <td>:</td>
-                      <td class="ps-2">12.85 x 10.17 x 8.60 mm</td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4">CROWN CUT</td>
-                      <td>:</td>
-                      <td class="ps-2"><?= $result['crown_cut'] ?></td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4">PAVILLION CUT</td>
-                      <td>:</td>
-                      <td class="ps-2"><?= $result['pavillion_cut'] ?></td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4 pt-4">ORIGIN</td>
-                      <td class="pt-4">:</td>
-                      <td class="ps-2 pt-4 fw-bold text-black"><?= $result['origin'] ?></td>
-                    </tr>
-                  </table>
-                  <div>
-                    <hr class="w-100 mt-5 border-2 text-dark">
-                  </div>
-                  <table>
-                    <h6 class="fw-bold fst-italic">TESTING METHODS & RESULTS</h6>
-                    <tr>
-                      <td class="pe-4">REFRACTIVE INDEX</td>
-                      <td>:</td>
-                      <td class="ps-2"><?= $result['refractive_index'] ?></td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4">SPECIFIC GRAVITY</td>
-                      <td>:</td>
-                      <td class="ps-2"><?= $result['specific_gravity'] ?></td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4">POLARISCOPE</td>
-                      <td>:</td>
-                      <td class="ps-2"><?= $result['polarisscope'] ?></td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4">SPECTROSCOPE 400-700NM</td>
-                      <td>:</td>
-                      <td class="ps-2"><?= $result['spectroscope'] ?></td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4">STANDARD FLUORESCENCE LW/SW</td>
-                      <td>:</td>
-                      <td class="ps-2"><?= $result['standard_fluorescence'] ?></td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4">DICHROSCOPE</td>
-                      <td>:</td>
-                      <td class="ps-2"><?= $result['dichroscope'] ?></td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4">COBALT/CHROMIUM FILTER</td>
-                      <td>:</td>
-                      <td class="ps-2"><?= $result['cobalt'] ?></td>
-                    </tr>
-                    <tr>
-                      <td class="pe-4">MICROSCOPE MAGNIFICATION 80X</td>
-                      <td>:</td>
-                      <td class="ps-2"><?= $result['microscope'] ?></td>
-                    </tr>
-                  </table>
+      <section id="landingContact" class="section-py bg-body landing-contact">
+        <div class="container">
+          <div class="col-lg-12">
+            <h4 class="text-center">RESULTS</h4>
+            <div class="card">
+              <div class="card-body">
+                <h5 class="mb-3 float-end">ONLINE VERIFICATION SERVICES - GFCO GEM LAB*</h5><br>
+                <table>
+                  <tr>
+                    <td class="pe-4">REPORT ID NBR</td>
+                    <td>:</td>
+                    <td class="ps-2 fw-bold text-black"><?= $result['nbr_id']; ?></td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4">CODE</td>
+                    <td>:</td>
+                    <td class="ps-2"><?= $result['code']; ?></td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4">DATE (DD/MM/YY)</td>
+                    <td>:</td>
+                    <td class="ps-2"><?= $db->date_format($result['date']); ?></td>
+                  </tr>
+                  <tr>
+                    <td class="pt-3"><img src="<?= $result['image_link'] ?>" alt="" width="150"></td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4 pt-3">FAMILY/SPECIE</td>
+                    <td class="pt-3">:</td>
+                    <td class="ps-2 pt-3 fw-bold text-black"><?= $result['cate_name'] ?></td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4">TRADE NAME</td>
+                    <td>:</td>
+                    <td class="ps-2 text-danger fw-bold"><?= $result['name'] ?></td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4">WEIGHT</td>
+                    <td>:</td>
+                    <td class="ps-2"><?= $result['p_weight'] ?> CARATS</td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4">COLOR</td>
+                    <td>:</td>
+                    <td class="ps-2"><?= $result['var_name'] ?></td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4">SHAPE</td>
+                    <td>:</td>
+                    <td class="ps-2"><?= $result['shape'] ?></td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4">DEMESIONS</td>
+                    <td>:</td>
+                    <td class="ps-2">12.85 x 10.17 x 8.60 mm</td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4">CROWN CUT</td>
+                    <td>:</td>
+                    <td class="ps-2"><?= $result['crown_cut'] ?></td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4">PAVILLION CUT</td>
+                    <td>:</td>
+                    <td class="ps-2"><?= $result['pavillion_cut'] ?></td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4 pt-4">ORIGIN</td>
+                    <td class="pt-4">:</td>
+                    <td class="ps-2 pt-4 fw-bold text-black"><?= $result['origin'] ?></td>
+                  </tr>
+                </table>
+                <div>
+                  <hr class="w-100 mt-5 border-2 text-dark">
                 </div>
+                <table>
+                  <h6 class="fw-bold fst-italic">TESTING METHODS & RESULTS</h6>
+                  <tr>
+                    <td class="pe-4">REFRACTIVE INDEX</td>
+                    <td>:</td>
+                    <td class="ps-2"><?= $result['refractive_index'] ?></td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4">SPECIFIC GRAVITY</td>
+                    <td>:</td>
+                    <td class="ps-2"><?= $result['specific_gravity'] ?></td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4">POLARISCOPE</td>
+                    <td>:</td>
+                    <td class="ps-2"><?= $result['polarisscope'] ?></td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4">SPECTROSCOPE 400-700NM</td>
+                    <td>:</td>
+                    <td class="ps-2"><?= $result['spectroscope'] ?></td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4">STANDARD FLUORESCENCE LW/SW</td>
+                    <td>:</td>
+                    <td class="ps-2"><?= $result['standard_fluorescence'] ?></td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4">DICHROSCOPE</td>
+                    <td>:</td>
+                    <td class="ps-2"><?= $result['dichroscope'] ?></td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4">COBALT/CHROMIUM FILTER</td>
+                    <td>:</td>
+                    <td class="ps-2"><?= $result['cobalt'] ?></td>
+                  </tr>
+                  <tr>
+                    <td class="pe-4">MICROSCOPE MAGNIFICATION 80X</td>
+                    <td>:</td>
+                    <td class="ps-2"><?= $result['microscope'] ?></td>
+                  </tr>
+                </table>
               </div>
             </div>
           </div>
+        </div>
   </div>
   </section>
 <?php
-      } else {
-?>
-  <h1>Data not valid.</h1>
-<?php
-      }
     }
 ?>
 <!-- Results Us: End -->
